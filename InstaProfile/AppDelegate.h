3e0d2d32dbfeacf6414f21b894ed1b1fda91ad06
Amyr Farsad <amir.farsad@gmail.com>
2016-05-22 23:38:49 +0430

    Initial Commit

---
+++ InstaProfile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaProfile
+//
+//  Created by Amir Farsad on 5/22/16.
+//  Copyright © 2016 Emersad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
